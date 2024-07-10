40354d4574380c7dc47203f26b71e8bd81ac5f8e
saeyeonIm <75128077+saeyeonIm@users.noreply.github.com>
2024-07-10 10:17:39 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,20 +1,40 @@
 from collections import deque
 
-def solution(begin, target, words):
-    queue = deque([(begin, 0)])  # (단어, 단계 수)
-    visited = [False] * len(words)  # 단어의 방문 여부 기록
+def bfs(begin, target, words):
+    queue = deque()
+    queue.append((begin, 0)) # 처음단어, 전환수
+    
+    visited = [False] * len(words)
 
     while queue:
         word, step = queue.popleft()
 
         if word == target:
-            return step
+            return step    
+        
+        
+#                 for i in range(len(word)):
+#             for char in 'abcdefghijklmnopqrstuvwxyz':
+#                 new_word = list(word)
+#                 new_word[i] = char
+#                 new_word = ''.join(new_word)
 
+#                 if new_word in words and new_word not in visited:
+#                     visited.add(new_word)
+#                     queue.append((new_word, step + 1))
+        
         for i in range(len(word)):
             for char in 'abcdefghijklmnopqrstuvwxyz':
                 new_word = word[:i] + char + word[i+1:]
+                
+                # visited[words에서 word의 index]
                 if new_word in words and not visited[words.index(new_word)]:
                     visited[words.index(new_word)] = True
                     queue.append((new_word, step + 1))
 
-    return 0  # 변환할 수 없는 경우
+def solution(begin, target, words):
+    if target not in words:
+        return 0
+
+    answer = bfs(begin, target, words)
+    return answer
